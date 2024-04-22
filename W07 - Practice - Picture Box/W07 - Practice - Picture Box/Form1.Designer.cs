182c3cca39d35cef30b79f77ac3cf8a716d09047
evelinn11 <139670444+evelinn11@users.noreply.github.com>
2024-04-22 23:16:25 +0700

    Add files via upload

---
+++ W07 - Practice - Picture Box/W07 - Practice - Picture Box/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace W07___Practice___Picture_Box
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pBox_Hewan1 = new System.Windows.Forms.PictureBox();
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.btn_Prev = new System.Windows.Forms.Button();
+            this.btn_Change = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox_Hewan1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pBox_Hewan1
+            // 
+            this.pBox_Hewan1.Location = new System.Drawing.Point(156, 48);
+            this.pBox_Hewan1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pBox_Hewan1.Name = "pBox_Hewan1";
+            this.pBox_Hewan1.Size = new System.Drawing.Size(408, 307);
+            this.pBox_Hewan1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pBox_Hewan1.TabIndex = 0;
+            this.pBox_Hewan1.TabStop = false;
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.Location = new System.Drawing.Point(616, 187);
+            this.btn_Next.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(94, 54);
+            this.btn_Next.TabIndex = 1;
+            this.btn_Next.Text = "NEXT";
+            this.btn_Next.UseVisualStyleBackColor = true;
+            this.btn_Next.Click += new System.EventHandler(this.btn_Image_Click);
+            // 
+            // btn_Prev
+            // 
+            this.btn_Prev.Location = new System.Drawing.Point(30, 187);
+            this.btn_Prev.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Prev.Name = "btn_Prev";
+            this.btn_Prev.Size = new System.Drawing.Size(94, 54);
+            this.btn_Prev.TabIndex = 2;
+            this.btn_Prev.Text = "PREV";
+            this.btn_Prev.UseVisualStyleBackColor = true;
+            this.btn_Prev.Click += new System.EventHandler(this.btn_Prev_Click);
+            // 
+            // btn_Change
+            // 
+            this.btn_Change.Location = new System.Drawing.Point(318, 381);
+            this.btn_Change.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Change.Name = "btn_Change";
+            this.btn_Change.Size = new System.Drawing.Size(84, 23);
+            this.btn_Change.TabIndex = 3;
+            this.btn_Change.Text = "Change";
+            this.btn_Change.UseVisualStyleBackColor = true;
+            this.btn_Change.Click += new System.EventHandler(this.btn_Change_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 420);
+            this.Controls.Add(this.btn_Change);
+            this.Controls.Add(this.btn_Prev);
+            this.Controls.Add(this.btn_Next);
+            this.Controls.Add(this.pBox_Hewan1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBox_Hewan1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pBox_Hewan1;
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.Button btn_Prev;
+        private System.Windows.Forms.Button btn_Change;
+    }
+}
+
---
+++ W07 - Practice - Picture Box/W07 - Practice - Picture Box/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace W07___Practice___Picture_Box
+{
+    public partial class Form1 : Form
+    {
+        string[] hewanBuas = {"Badak", "Banteng", "Beruang", "Buaya", "Cheetah", "Gorila", "Hiu", "Hyena", "kalajengking", "Komodo", "Piranha", "Rubah", "Serigala", "Singa", "Ular"};
+        string[] hewanPeliharaan = { "Anjing", "Bebek", "BurungKakakTua", "Domba", "Ferret", "GuineaPig", "Hamster", "Kambing", "Keledai", "Kelinci", "Koi", "LandakMini", "Merpati", "Otter", "Sapi" };
+        int countP;
+        int countB;
+        string mode = "peliharaan";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pBox_Hewan1.Image = new Bitmap("D:\\UC\\Semester 2\\Application Development\\Hewan Peliharaan\\Anjing.jpg");
+            countP = 0;
+        }
+
+        private void btn_Image_Click(object sender, EventArgs e)
+        {
+            //string alamat = "Y:\\Hewan Peliharaan\\Hamster.jpg";
+            //pBox_Hewan1.Image = new Bitmap(alamat);
+            if (mode == "peliharaan")
+            {
+                if (countP < 14)
+                {
+                    countP++;
+                    pBox_Hewan1.Image = new Bitmap("D:\\UC\\Semester 2\\Application Development\\Hewan Peliharaan\\" + hewanPeliharaan[countP] + ".jpg");
+                }
+            }
+            else
+            {
+                if (countB < 14)
+                {
+                    countB++;
+                    pBox_Hewan1.Image = new Bitmap("D:\\UC\\Semester 2\\Application Development\\Hewan Buas\\" + hewanBuas[countB] + ".jpg");
+                }
+            }
+        }
+
+        private void btn_Prev_Click(object sender, EventArgs e)
+        {
+            if (mode == "peliharaan")
+            {
+                if (countP > 0)
+                {
+                    countP--;
+                    pBox_Hewan1.Image = new Bitmap("D:\\UC\\Semester 2\\Application Development\\Hewan Peliharaan\\" + hewanPeliharaan[countP] + ".jpg");
+                }
+            }
+            else
+            {
+                if (countB > 0)
+                {
+                    countB--;
+                    pBox_Hewan1.Image = new Bitmap("D:\\UC\\Semester 2\\Application Development\\Hewan Buas\\" + hewanBuas[countB] + ".jpg");
+                }
+            }
+        }
+
+        private void btn_Change_Click(object sender, EventArgs e)
+        {
+            if (mode == "peliharaan")
+            {
+                mode = "buas";
+                pBox_Hewan1.Image = new Bitmap("D:\\UC\\Semester 2\\Application Development\\Hewan Buas\\" + hewanBuas[countB] + ".jpg");
+            }
+            else
+            {
+                mode = "peliharaan";
+                pBox_Hewan1.Image = new Bitmap("D:\\UC\\Semester 2\\Application Development\\Hewan Peliharaan\\" + hewanPeliharaan[countP] + ".jpg");
+            }
+        }
+    }
+}
