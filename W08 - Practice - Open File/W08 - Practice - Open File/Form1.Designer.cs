b17dd80a03db19e230e1b7793639e422b20ca6a1
evelinn11 <139670444+evelinn11@users.noreply.github.com>
2024-04-22 23:24:29 +0700

    Add files via upload

---
+++ W08 - Practice - Open File/W08 - Practice - Open File/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace W08___Practice___Open_File
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Baca = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Baca
+            // 
+            this.btn_Baca.Location = new System.Drawing.Point(147, 86);
+            this.btn_Baca.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Baca.Name = "btn_Baca";
+            this.btn_Baca.Size = new System.Drawing.Size(88, 37);
+            this.btn_Baca.TabIndex = 0;
+            this.btn_Baca.Text = "Baca File";
+            this.btn_Baca.UseVisualStyleBackColor = true;
+            this.btn_Baca.Click += new System.EventHandler(this.btn_Baca_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 234);
+            this.Controls.Add(this.btn_Baca);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Baca;
+    }
+}
+
---
+++ W08 - Practice - Open File/W08 - Practice - Open File/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W08___Practice___Open_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Baca_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "D:\\UC\\Semester 2\\Application Development\\Folder TXT"; //set default folder yang akan dibuka
+            ofd.Filter = "EXO saranghaja (*.txt)|*.txt|All Files (*.*)|*.*"; //filter jenis file di folder
+            ofd.FilterIndex = 2; //set default filter
+            ofd.ShowDialog();
+            MessageBox.Show(ofd.FileName);
+
+            StreamReader sr = new StreamReader(ofd.FileName); //membaca file
+            string line = sr.ReadLine(); //membaca 1 baris di file
+            while (line != null)
+            {
+                MessageBox.Show(line);
+                line = sr.ReadLine();
+            }
+            sr.Close(); //jika sudah tidak dipakai 
+
+            StreamWriter sw = new StreamWriter(ofd.FileName);
+            sw.WriteLine("Baekhyun;Seoul;01012322445");
+            sw.WriteLine("Juyeon;Seoul;010465432");
+            sw.Close();
+        }
+    }
+}
