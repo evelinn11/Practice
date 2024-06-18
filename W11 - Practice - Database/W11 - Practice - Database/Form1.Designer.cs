3faa637c32719796763a00b25ecf0bb8a5993469
evelinn11 <139670444+evelinn11@users.noreply.github.com>
2024-06-18 21:09:40 +0700

    Add files via upload

---
+++ W11 - Practice - Database/W11 - Practice - Database/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace W11___Practice___Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dGV_Team = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV_Team)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dGV_Team
+            // 
+            this.dGV_Team.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dGV_Team.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGV_Team.Location = new System.Drawing.Point(18, 67);
+            this.dGV_Team.Name = "dGV_Team";
+            this.dGV_Team.RowHeadersWidth = 82;
+            this.dGV_Team.RowTemplate.Height = 33;
+            this.dGV_Team.Size = new System.Drawing.Size(1995, 917);
+            this.dGV_Team.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2047, 1015);
+            this.Controls.Add(this.dGV_Team);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dGV_Team)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dGV_Team;
+    }
+}
+
---
+++ W11 - Practice - Database/W11 - Practice - Database/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace W11___Practice___Database
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection sqlConnect;
+        MySqlCommand sqlCommand;
+        MySqlDataAdapter sqlDataAdapter;
+        string sqlQuery;
+        DataTable dtTeam;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dtTeam = new DataTable();
+            sqlConnect = new MySqlConnection("server=localhost;uid=student;pwd=isbmantap;database=premier_league");
+            sqlQuery = "SELECT t.team_id AS 'Team ID', t.team_name AS 'Team Name', t.home_stadium AS 'Home Stadium', t.capacity AS 'Capacity', t.city AS 'City', ma.manager_name AS 'Manager Name', ass.manager_name AS 'Assistant Manager Name', p.player_name AS 'Captain Name' FROM team t JOIN manager ma ON t.manager_id = ma.manager_id JOIN manager ass ON t.assmanager_id = ass.manager_id JOIN player p ON p.player_id = t.captain_id";
+            //sqlQuery = "SELECT m.match_id, m.match_date, t.team_name as 'Team Home', t2.team_name as 'Team Away' FROM `match` m JOIN team t ON t.team_id = m.team_home JOIN team t2 ON t2.team_id = m.team_away";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlDataAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlDataAdapter.Fill(dtTeam);
+            dGV_Team.DataSource = dtTeam;
+        }
+    }
+}
